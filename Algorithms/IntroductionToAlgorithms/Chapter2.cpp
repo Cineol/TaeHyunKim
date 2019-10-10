6f65d895107a8d7deb9721a7661dda7bdd1f9b4c
Cineol <55614670+Cineol@users.noreply.github.com>
2019-10-10 22:30:13 +0900

    Update Chapter2.cpp
    Add a practice 2.1-2

--- Algorithms/IntroductionToAlgorithms/Chapter2.cpp
+++ Algorithms/IntroductionToAlgorithms/Chapter2.cpp
@@ -20,3 +20,22 @@ void Chapter2::Example_2_1_InsertionSort(vector<int> src)
 		cout << sortResult << endl;
 	}
 }
+
+void Chapter2::Practice_2_1_2_InsertionSort(vector<int> src)
+{
+	vector<int> mound = src;
+	int key;
+	int after = 0;
+ 	for (int card = mound.size()-1; card > 0; card--) {
+		key = mound[card-1];
+		after = card;
+		while (after <=mound.size()-1 &&mound[after]>key) {
+			mound[after-1] = mound[after];
+			after++;
+		}
+		mound[after-1] = key;
+	}
+	for (auto sortResult : mound) {
+		cout << sortResult << enld;
+	}
+}
